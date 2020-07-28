6ec1644f5b26b599de9180b0b2d84f356ecbab78
Ziguard <gef56150@gmail.com>
2020-07-28 08:59:24 +0200

    feat
    init back

---
+++ back/src/main/java/com/coffefreaks/timberwallee/TimberWalleEApplication.java
@@ -0,0 +1,13 @@
+package com.coffefreaks.timberwallee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimberWalleEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimberWalleEApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/coffefreaks/timberwallee/TimberWalleEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffefreaks.timberwallee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimberWalleEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
