3342a34170cce47832a540c43b605fa3ec0ad360
Juliette <juliette@saint-jalme.fr>
2020-07-29 14:47:40 +0200

    segment, icon pad and header

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Button, Container, Header} from "semantic-ui-react";
+import {Button, Container, Segment, Image, Grid} from "semantic-ui-react";
 import './App.css';
+import logo from '../src/logowithoutslogan.svg';
 
 function App() {
   const boomer = () => {
@@ -8,10 +9,60 @@ function App() {
   };
 
   return (
-    <Container className="App">
-      <Header as='h1'>Timber WALL-E Simulator</Header>
-      <Button onClick={boomer}>React Semantic UI is installed :D</Button>
+    <Container fluid="1">
+      <Segment.Group>
+        <Segment><Image src={logo} size='tiny'/></Segment>
+        <Segment>
+         <Grid  columns={3} aligned>
+            <Grid.Row >
+              <Grid.Column >
+                  <button class="ui button basic massive no-border" size="huge"><i class="angle left icon fitted big"></i></button>
+                  </Grid.Column>
+                    <Grid.Column textAlign='center'>
+                      <h2>Nom de la map</h2>
+                    </Grid.Column>
+                    <Grid.Column textAlign='right'>
+                  <button centered class="ui button basic massive no-border" size="huge"><i class="angle right icon fitted big"></i></button>
+              </Grid.Column>
+            </Grid.Row>
+            </Grid>
+          <Grid verticalAlign='middle' columns={16} centered>
+            <Grid.Row>
+              <Grid.Column>
+              <button class="ui button no-border basic" ><i class="arrow left icon icon-orange huge"></i></button>
+              </Grid.Column>
+              <Grid.Column >
+              <button class="ui button no-border basic"><i class="arrow up icon icon-orange huge"></i></button>
+                <br />
+                <button class="ui button no-border basic"><i class="circle icon icon-orange massive"></i></button>
+                <br/>
+                <button class="ui button no-border basic"><i class="arrow down icon icon-orange huge"></i></button>
+              </Grid.Column>
+              <Grid.Column>
+              <button class="ui button no-border basic"><i class="arrow right icon icon-orange huge"></i></button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+        <Segment>
+          <Grid >
+            <Grid.Row verticalAlign='middle' columns={5} centered>
+              <Grid.Column floated='left' width={5}>
+                <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+              </Grid.Column>*
+              <Grid.Column></Grid.Column>
+              <Grid.Column floated='right' width={5}>
+                <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+        <Segment>
+          
+        </Segment>
+      </Segment.Group>
     </Container>
+
   );
 }
 
---
+++ front/src/components/SegmentExampleCompactGroup.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Grid, Image } from 'semantic-ui-react'
+
+const GridExampleVerticalAlignment = () => (
+  <Grid verticalAlign='middle' columns={4} centered>
+    <Grid.Row>
+      <Grid.Column>
+
+      </Grid.Column>
+      <Grid.Column>
+
+        <br />
+
+      </Grid.Column>
+      <Grid.Column>
+
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+)
+
+export default GridExampleVerticalAlignment
