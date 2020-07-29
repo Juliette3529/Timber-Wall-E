2c5d37f3c6924c6c627135745c48f8941994ab87
Ziguard <gef56150@gmail.com>
2020-07-29 17:09:32 +0200

    feat : Add javadoc in Cartography & Scenario Service

--- back/src/main/java/com/coffefreaks/timberwallee/model/Move.java
+++ back/src/main/java/com/coffefreaks/timberwallee/model/Move.java
@@ -2,6 +2,8 @@ package com.coffefreaks.timberwallee.model;
 
 import com.coffefreaks.timberwallee.model.Enum.Direction;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 
 
@@ -10,12 +12,25 @@ public class Move {
     @NotEmpty
     private Direction direction;
 
+    @NotEmpty
+    @Min(value = 1)
+    @Max(value = 50)
+    private int distance;
+
     public void setDirection(Direction direction) {
         this.direction = direction;
     }
 
     public Direction getDirection() {
         return direction;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
     }
 
 }
