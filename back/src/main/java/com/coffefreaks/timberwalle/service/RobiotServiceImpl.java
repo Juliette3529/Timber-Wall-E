0c42c7ad7979c8f5da00425e6e6d791f6c269bbb
AudreyP <audrey.patin@imie.fr>
2020-07-30 17:12:40 +0200

    feat(moving): fix moving methode to use PUT and fix double format

--- back/src/main/java/com/coffefreaks/timberwalle/service/RobiotServiceImpl.java
+++ back/src/main/java/com/coffefreaks/timberwalle/service/RobiotServiceImpl.java
@@ -9,10 +9,14 @@ import com.coffefreaks.timberwalle.service.Interface.RobiotService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Locale;
 
 @Service
 public class RobiotServiceImpl implements RobiotService {
@@ -74,12 +78,15 @@ public class RobiotServiceImpl implements RobiotService {
                 return false;
             }
         }
-        RobiotRequest request = new RobiotRequest(302, String.format("%1$,.2f, %2$,.2f", dest.getPositionX(), dest.getPositionY()));
+        RobiotRequest request = new RobiotRequest(302,
+                            String.format(Locale.ROOT, "%1$.2f,%2$.2f", dest.getPositionX(), dest.getPositionY()));
         String url = robiotEndpoint + "/302";
         logger.trace("move - url = {}", url);
+        HttpEntity<RobiotRequest> entity = new HttpEntity<>(request);
 
         boolean result = false;
-        ResponseEntity<RobiotResponse> response = clientRobiot.getForEntity(url, RobiotResponse.class);
+        ResponseEntity<RobiotResponse> response = clientRobiot.exchange(url, HttpMethod.PUT,
+                                        entity, RobiotResponse.class);
         if (response.getStatusCode() == HttpStatus.OK && response.getBody() != null) {
             result = true;
         } else {
