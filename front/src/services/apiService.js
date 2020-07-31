c127ffab7924a837de57fceee5ea1937a42c4646
Jessy <git@bellanger.me>
2020-07-31 17:32:40 +0200

    develop-avecApiService - feat - Added robot movement with some restrictions (obstacles and bounds)

--- front/src/services/apiService.js
+++ front/src/services/apiService.js
@@ -1,22 +1,17 @@
 let url = 'http://localhost:8090/move';
-let myBody = {
-    "positionX": '1.0',
-    "positionY": '1.0'
-  } 
-const functions = { postMove : async () => {
-    const response = await fetch(url, {
-      method: 'POST',
-      body : JSON.stringify( myBody),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const myJson = await response.json(); 
-    console.log(myJson);
-    console.log('dfvqdfvqfvqfvs');
-    //ici traitement ou ailleurs d'ailleurs juste return le myJSON
-    return myJson
-  }
+
+const functions = {
+    postMove : async (positionX, positionY) => {
+        const response = await fetch(url, {
+            method: 'POST',
+            body : JSON.stringify({positionX, positionY}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        return await response.json();
+    }
 }
 
 export default functions;
