036c7c1cf5f688b2f8645904978f484092a40cb6
Ziguard <gef56150@gmail.com>
2020-07-31 12:20:13 +0200

    CI: Change url for cores

--- back/src/main/java/com/coffefreaks/timberwalle/TimberWallEApplication.java
+++ back/src/main/java/com/coffefreaks/timberwalle/TimberWallEApplication.java
@@ -18,7 +18,7 @@ public class TimberWallEApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/move").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
 			}
 		};
 	}
